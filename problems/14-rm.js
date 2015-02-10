6794e869901e6c43f7a8a57989b6a2c3b9191d5f
isaacs <i@izs.me>
2015-02-09 19:45:09 -0800

    npm rm exercise

--- problems/14-rm.js
+++ problems/14-rm.js
@@ -5,7 +5,8 @@ var fs = require('fs')
 var path = require('path')
 
 exports.problem = function () {
-  // Capture the set of deps
+  if (!shop.cwd())
+    return ''
 
   return function () { /*
 If you have a way to put stuff there, then naturally, you'll one
@@ -14,16 +15,49 @@ day need a way to delete them.
 Enter the `npm rm` command (aka `npm uninstall` if you prefer to
 type tings out long-hand).
 
-Remove one of the deps.
+Remove all the deps!  But, make sure that you don't keep depending on them.
 
 Just like you can use `--save` on installing packages, you can also
 use `--save` when removing packages, to also remove them from your
 package.json file.
 
-When you've removed a dependency, type `how-to-npm verify` to move on.
+When you've removed your dependencies, type `how-to-npm verify` to move on.
 */}.toString().split('\n').slice(1,-1).join('\n')
 }
 
 exports.verify = function (args, cb) {
-  console.log('TODO: make sure that the publish worked')
+  var cwd = shop.cwd()
+  if (!cwd)
+    return cb(false)
+
+  var pkg = require(cwd + '/package.json')
+  var deps = Object.keys(pkg.dependencies || {})
+  var nm
+  try {
+    var nm = fs.readdirSync(path.resolve(cwd, 'node_modules'))
+    nm = nm.filter(function (n) {
+      return !/^\./.test(n)
+    })
+  } catch (er) {
+    nm = []
+  }
+
+  if (nm.length) {
+    console.log('Looks like there are some deps still hanging around')
+    return cb(false)
+  }
+
+  if (deps.length) {
+    console.log('You removed the files, but not the entries in package.json')
+    return cb(false)
+  }
+
+  console.log(function () {/*
+Awesome!  You have removed the packages from your node_modules folder,
+and also updated your package.json file to reflect that you're no longer
+depending on them.
+
+Well done.
+*/}.toString().split('\n').slice(1,-1).join('\n'))
+  return cb(true)
 }
