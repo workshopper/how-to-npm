791f0750d477fb3a57dffa5af200debc6590cf6e
Lin Clark <lin.w.clark@gmail.com>
2015-05-21 16:47:20 -0400

    switch from @isaacs/once to @linclark/pkg

--- problems/04-install-a-module.js
+++ problems/04-install-a-module.js
@@ -21,13 +21,13 @@ The registry that we're using for this tutorial is a tiny version of
 the one at https://registry.npmjs.org.  So you might find that it only has
 a small number of things.
 
-Let's start out by installing the "@isaacs/once" module.
+Let's start out by installing the "@linclark/pkg" module.
 
 Run `$ADVENTURE_COMMAND verify` once you're done.
 */}.toString().split('\n').slice(1,-1).join('\n')
 }
 
-//exports.solution = 'npm install @isaacs/once'
+//exports.solution = 'npm install @linclark/pkg'
 
 exports.verify = function (args, cb) {
   var cwd = shop.cwd()
@@ -35,13 +35,13 @@ exports.verify = function (args, cb) {
     return cb(false)
 
   try {
-    var once = require(cwd + '/node_modules/@isaacs/once')
+    var pkg = require(cwd + '/node_modules/@linclark/pkg')
   } catch (er) {
     console.log('Uh oh!  Looks like it didn\'t install right.\n'+
                 'The error I got was: \n' +
                 (er.stack || er.message) + '\n' +
-                'Make sure you use the `npm install @isaacs/once` command\n' +
-                'to install the `@isaacs/once` module.')
+                'Make sure you use the `npm install @linclark/pkg` command\n' +
+                'to install the `@linclark/pkg` module.')
     return cb(false)
   }
 
