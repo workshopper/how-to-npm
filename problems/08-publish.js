0d6bc3b5e0f5fe1b72c8d114add7addcf95b548d
Mike Brett <mdouglasbrett@icloud.com>
2015-06-23 18:17:09 +0100

    Add cb to publish.js
    Hi there :)
    
    RE: Issue #7 (Publish verify passes without actually publishing). I also had this problem and noticed that the `if (err)` block did not return. 
    
    Hope my change makes it in!
    
    All the best, 
    
    Mike

--- problems/08-publish.js
+++ problems/08-publish.js
@@ -20,7 +20,7 @@ npm users to publish their modules and share them with the world.
 
 Packages get into the registry by using the `npm publish` command.
 
-Try it now.  There's not much too it.
+Try it now.  There's not much to it.
 
 (Make sure you're still in the right project directory, though.  If you
 publish something by mistake, you can remove it, but there's no guarantee
@@ -47,6 +47,7 @@ exports.verify = function (args, cb) {
                   'It looks like you didn\'t successfully publish the ' +
                   name + '\n' +
                   'package.  Try again!\n')
+      return cb(false)
     }
 
     console.log(function () {/*
