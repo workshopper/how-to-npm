c00223f9042ad401471c8ea8fbab79a7a5160bbf
Pippin Lee <pippinblee@gmail.com>
2015-11-09 20:37:50 -0500

    fix #24 #43; add more detail about npm scripts and concise info about goals of problem

--- problems/06-npm-test.js
+++ problems/06-npm-test.js
@@ -18,13 +18,14 @@ If you look at the package.json file, it has this rather odd bit in it:
     "test": "echo \"Error: no test specified\" && exit 1"
   },
 
-npm can be used as a task runner, and almost every module and project
-will have a test script that runs to make sure everything is good.  In
-order to help remind you to do this, npm puts a "always failing" test
-in there by default.
+npm can be used as a task runner with its "scripts" property.  Almost
+every module and project will have a test script that runs to make
+sure everything is good.  In order to help remind you to do this, npm
+puts an "always failing" test in there by default.
 
-First, create a file called `test.js`.  It doesn't have to do anything,
-really.  (This is npm class, not testing class.)  But it has to exit
+First, create a file called `test.js`.  For the purposes of this workshop
+you can leave it blank, but this would be where you'd write your
+tests (this is npm class, not testing class).  The test has to exit
 without throwing an error, or else the test fails.
 
 Then, edit your `package.json` file to make your scripts section look like
@@ -35,6 +36,12 @@ this instead:
   },
 
 Once that's done, run `how-to-npm verify` to check your work.
+
+Additional info: If you wanted to actually run any tests you'd written in
+`test.js` with the "test" script, you'd run `npm test`.
+
+The docs for npm's scripts property can be found here:
+https://docs.npmjs.com/misc/scripts
 */}.toString().split('\n').slice(1,-1).join('\n')
 }
 
