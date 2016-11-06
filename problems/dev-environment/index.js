997014c7ff12bf4684b3b22a67e0ab549e5328ce
Martin Heidegger <martin.heidegger@gmail.com>
2016-11-07 02:06:08 +0900

    Replaced translation in dev environment

--- problems/dev-environment/index.js
+++ problems/dev-environment/index.js
@@ -2,46 +2,40 @@ var shop = require('../../')
 var fs = require('fs')
 var path = require('path')
 
+exports.problem = {
+  file: path.join(__dirname, 'problem.{lang}.txt')
+}
+
 exports.init = function (workshopper) {
-  this.problem = {
-    file: path.join(__dirname, 'problem.{workshopper.lang}.txt')
-  }
+  this.__ = workshopper.i18n.__
 }
 
 exports.verify = function (args, cb) {
   var datadir = shop.datadir
   var cwd = process.cwd()
-  if (path.resolve(cwd, 'problems', path.basename(__filename)) === path.resolve(__filename)) {
-    console.log('It looks like you are in the root of the workshopper.\n' +
-                'That is not wise.  Please make a new dir, and use that.')
+  var __ = this.__
+  if (path.resolve(cwd, 'problems', 'dev-environment', path.basename(__filename)) === path.resolve(__filename)) {
+    console.log(__('dev-environment.err_workshopper'))
+    console.log(__('dev-environment.hint'))
     return cb(false)
   }
 
   if (cwd === process.env.HOME || cwd === process.env.USERPROFILE) {
-    console.log('It looks like you are in your home directory.\n' +
-                'That is not wise.  Please make a new dir, and use that.')
+    console.log(__('dev-environment.err_home'))
+    console.log(__('dev-environment.hint'))
     return cb(false)
   }
-
-  console.log(
-    'Congratulations!\n' +
-    'You have a development environment.\n' +
-    '\n' +
-    'From here on out, make sure to run the workshop in this dir\n' +
-    '\n' +
-    'You might notice that a `.npmrc` file will show up in there.\n' +
-    'That normally would not be required, but the workshop uses it\n' +
-    'to mock out the parts that would normally talk to the internet.'
-  )
 
   // Save the cwd so that we can ensure we're in the right place from now on
   fs.writeFileSync(path.resolve(datadir, 'cwd'), cwd)
 
   // Write the .npmrc file telling it to always use our local registry.
-  var conf = fs.readFileSync(path.resolve(__dirname, '..', 'workshop-npmrc'), 'utf8')
+  var conf = fs.readFileSync(path.resolve(__dirname, '..', '..', 'workshop-npmrc'), 'utf8')
   conf = conf.replace(/%USERCONFIG%/g, path.resolve(datadir, 'npmrc'))
 
   fs.writeFileSync(path.resolve(cwd, '.npmrc'), conf)
 
-  return cb(true)
+  return cb(null, true, {
+    file: path.join(__dirname, 'success.{lang}.txt')
+  })
 }
