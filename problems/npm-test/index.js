354a0e5bd520b136dc5447e32827a22269b37446
Martin Heidegger <martin.heidegger@gmail.com>
2016-11-07 03:21:23 +0900

    Translated and fixed npm-test

--- problems/npm-test/index.js
+++ problems/npm-test/index.js
@@ -1,42 +1,43 @@
 var path = require('path')
 var shop = require('../../')
 
+exports.problem = {
+  file: path.join(__dirname, 'problem.{lang}.txt')
+}
+
 exports.init = function (workshopper) {
-  this.problem = {
-    file: path.join(__dirname, 'problem.{workshopper.lang}.txt')
-  }
+  this.__ = workshopper.i18n.__
 }
 
 exports.verify = function (args, cb) {
   var cwd = shop.cwd()
   if (!cwd) return cb(false)
 
+  var __ = this.__
+
   var pj = require(cwd + '/package.json')
 
   if (!pj.scripts || !pj.scripts.test) {
-    console.log('Oops!  You don\'t have a `scripts.test` section in your\n' +
-                'package.json file.  Edit it, and try again.')
+    console.log(__('npm-test.section_missing'))
     return cb(false)
   }
 
   // try running the test!
   var exec = require('child_process').exec
 
-  console.log('Running your test script...\n\n')
+  console.log(__('npm-test.running') + '\n\n')
   exec('npm test --color=always', function (er, stdout, stderr) {
     process.stdout.write(stdout)
     process.stderr.write(stderr)
 
-    console.log('\n\n...done!')
+    console.log('\n\n' + __('npm-test.running_done'))
 
     if (er) {
-      console.log('Uh oh!  The test failed!\n' +
-                  'Try creating a test that actually works.')
+      console.log(__('npm-test.error'))
       return cb(false)
     }
 
-    console.log('Congratulations!  You wrote a test that passes!\n' +
-                'Writing a test that is actually GOOD is left for another time.')
+    console.log(__('npm-test.success'))
     return cb(true)
   })
 }
