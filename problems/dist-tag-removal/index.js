ef04e14939dd2606ea99a476dfc6f20e75c36b53
Martin Heidegger <martin.heidegger@gmail.com>
2016-11-08 02:56:52 +0900

    Added Fixed and translated dist-tag-removal.

--- problems/dist-tag-removal/index.js
+++ problems/dist-tag-removal/index.js
@@ -2,25 +2,28 @@ var path = require('path')
 var reg = require('../../lib/registry.js')
 var shop = require('../../')
 
+exports.problem = {
+  file: path.join(__dirname, 'problem.{lang}.txt')
+}
+
 exports.init = function (workshopper) {
-  this.problem = {
-    file: path.join(__dirname, 'problem.{workshopper.lang}.txt')
-  }
+  this.__ = workshopper.i18n.__
+  reg.run('dist-tag')
 }
 
 exports.verify = function (args, cb) {
   var cwd = shop.cwd()
   if (!cwd) return cb(false)
 
-  var pkg = require(cwd + '/package.json')
+  var __ = this.__
+  var pkg = require(path.join(cwd, 'package.json'))
   var name = pkg.name
 
-  var body = require(shop.datadir + '/registry/' + name + '/body.json')
+  var body = require(path.join(shop.datadir, 'registry', name, 'body.json'))
   var dt = body['dist-tags']
   var tags = Object.keys(dt)
   if (tags.length > 1) {
-    console.log('Uh oh, looks like you have some extra dist-tags there.')
-    console.log('Use `npm help dist-tag` to learn how to remove them.')
+    console.log(__('dist-tag-removal.too_many'))
     return cb(false)
   }
 
@@ -36,19 +39,12 @@ exports.verify = function (args, cb) {
   }
 
   if (dt.latest === mostRecentVersion) {
-    console.log('Oops!  Your "latest" tag still points at the most recent\n' +
-                'release, %s.\n' +
-                'Point that somewhere else, and re-run `how-to-npm verify`\n' +
-                'Use `npm help dist-tag` to learn more about how to do it.',
-                mostRecentVersion)
+    console.log(__('dist-tag-removal.version_mismatch', {version: mostRecentVersion}))
     return cb(false)
   }
 
-  console.log(function () { /*
-Congratulations!  You're a dist-tagging pro!
-
-Run `how-to-npm` to move on to the next exercise.
-  */ }.toString().split('\n').slice(1, -1).join('\n'))
   reg.kill()
-  return cb(true)
+  return cb(null, true, {
+    file: path.join(__dirname, 'success.{lang}.txt')
+  })
 }
