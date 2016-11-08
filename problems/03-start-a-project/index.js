c6bd8dd3b825b8871d735ca04fc6b446d849ceed
Daijiro Wachi <daijiro.wachi@gmail.com>
2016-11-08 11:16:52 +0100

    Fix the init process in start-a-project

--- problems/03-start-a-project/index.js
+++ problems/03-start-a-project/index.js
@@ -1,11 +1,16 @@
 var shop = require('../../')
+var reg = require('../../lib/registry.js')
 var fs = require('fs')
 var path = require('path')
 
 exports.init = function (workshopper) {
-  this.problem = {
-    file: path.join(__dirname, 'problem.{workshopper.lang}.txt')
-  }
+  this.__ = workshopper.i18n.__
+  this.lang = workshopper.i18n.lang
+  reg.run('start-a-project')
+}
+
+exports.problem = {
+  file: path.join(__dirname, 'problem.{lang}.txt')
 }
 
 exports.verify = function (args, cb) {
