9827f008f886a62fea5f88dce398685b27d1f8fa
Daijiro Wachi <daijiro.wachi@gmail.com>
2016-11-08 11:56:32 +0100

    Move en texts of start-a-project into i18n

--- problems/03-start-a-project/index.js
+++ problems/03-start-a-project/index.js
@@ -15,42 +15,29 @@ exports.problem = {
 
 exports.verify = function (args, cb) {
   var cwd = shop.cwd()
-  if (!cwd) return cb(false)
+  var __ = this.__
 
   try {
     require(cwd + '/package.json')
   } catch (er) {
-    console.log('No package.json found.\n' +
-                'Please run `npm init` in your working directory.')
+    console.log(__('start-a-project.no_package'))
     return cb(false)
   }
 
   if (/^extracredit$/i.test(args[0] + args[1])) {
     try {
       fs.readFileSync(path.resolve(cwd, '.git', 'config'))
-      console.log('EXTRA CREDIT!  Nicely done.\n')
+      console.log(__('start-a-project.extra_credit'))
     } catch (er) {
-      console.log('Well, you got everything except the git part.\n' +
-                  'The convention is one git repo per module, so\n' +
-                  'you usually ought to run `git init` at the root\n' +
-                  'of your project.\n' +
-                  'Remove the .git folder from wherever you were,\n' +
-                  'and run `git init` in ' + cwd)
+      console.log(fs.readFileSync(path.join(__dirname, 'suggestion.' + this.lang() + '.txt'), 'utf8') + cwd)
       return cb(false)
     }
   } else {
-    console.log('For extra credit, try also setting up this dir as\n' +
-                'a git repository.\n\n' +
-                'The convention is to have a single git repo for each\n' +
-                'module or project.  Use the `git init` command to set\n' +
-                'up your working dir as a git project.\n\n' +
-                'Then, run `how-to-npm verify extra credit`\n\n')
+    console.log(fs.readFileSync(path.join(__dirname, 'continue.' + this.lang() + '.txt'), 'utf8'))
   }
 
-  console.log(
-    'Congratulations!\n' +
-    'You created a project!  View the package.json file to see it.\n'
-  )
+  console.log(__('start-a-project.success'))
+
 
   return cb(true)
 }
