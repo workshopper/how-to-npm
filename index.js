280825d76a7142393242739922382c5a35784218
Daijiro Wachi <daijiro.wachi@gmail.com>
2016-11-09 22:22:13 +0100

    Add ja lang

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 var adventure = require('workshopper-adventure/adventure')
 var shop = module.exports = adventure({
   name: 'how-to-npm',
-  languages: ['en'],
+  languages: ['en', 'ja'],
   appDir: __dirname,
   menu: {
     bg: 'white',
